2c0f10a84cfa1d2dc54901aada599dc1059e9cf1
azaka <yukkuri.shrine@gmail.com>
2015-06-23 23:35:28 +0800

    Create .travis.sh

---
+++ .travis.sh
@@ -0,0 +1,34 @@
+#/bin/bash
+
+download_extract() {
+	aria2c -x 16 $1 -o $2
+	tar -xf $2
+}
+
+travis_before_install() {
+	git submodule update --init --recursive
+}
+
+travis_install() {
+	sudo apt-get install lib32stdc++6 lib32bz2-1.0 -qq
+	download_extract https://github.com/xsacha/SymbianGCC/releases/download/4.8.3/gcc4.8.3_x86-64.tar.bz2 compiler.tar.bz2
+	download_extract https://github.com/xsacha/SymbianGCC/releases/download/4.8.3/ndk-new.tar.bz2 ndk.tar.bz2
+	export EPOCROOT=$(pwd)/SDKs/SymbianSR1Qt474/ SBS_GCCE483BIN=$(pwd)/gcc4.8.3_x86-64/bin
+	cp ffmpeg/symbian/armv6/lib/* $EPOCROOT/epoc32/release/armv5/urel/
+}
+
+travis_script() {
+	export EPOCROOT=$(pwd)/SDKs/SymbianSR1Qt474/ SBS_GCCE483BIN=$(pwd)/gcc4.8.3_x86-64/bin
+	PATH=$SBS_GCCE483BIN:$(pwd)/tools/sbs/bin:$EPOCROOT/epoc32/tools:$EPOCROOT/bin:$(pwd)/tools/sbs/linux-x86_64-libc2_15/bin:$PATH
+	QMAKE_ARGS="CONFIG+=no_assets" ./b.sh
+
+}
+
+travis_after_success() {
+
+}
+
+set -e
+set -x
+
+$1;

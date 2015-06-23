31e07aa6e87d6e611c2dcd423e21d36db62ebd3f
azaka <yukkuri.shrine@gmail.com>
2015-06-24 00:34:35 +0800

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -10,6 +10,7 @@ travis_before_install() {
 }
 
 travis_install() {
+	sudo apt-get update
 	sudo apt-get install lib32stdc++6 lib32bz2-1.0 -qq
 	download_extract https://github.com/xsacha/SymbianGCC/releases/download/4.8.3/gcc4.8.3_x86-64.tar.bz2 compiler.tar.bz2
 	download_extract https://github.com/xsacha/SymbianGCC/releases/download/4.8.3/ndk-new.tar.bz2 ndk.tar.bz2

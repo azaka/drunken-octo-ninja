efb3ad8f6466c34a1f5e1ef7d42c5e43359d652e
azaka <yukkuri.shrine@gmail.com>
2015-06-24 14:24:06 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <QtCore/QCoreApplication>
-#include <libavutil/avutil.h>
+#include <libavformat/avformat.h>
 #include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    qDebug() << avutil_license();
+    AVFormatContext* context = avformat_alloc_context();
     
     return a.exec();
 }

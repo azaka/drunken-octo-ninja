592bf16b2336a09158764f52368dee36691d02ad
azaka <yukkuri.shrine@gmail.com>
2015-10-13 23:16:53 +0800

    Update b.sh

--- b.sh
+++ b.sh
@@ -2,3 +2,11 @@
 
 qmake CONFIG+=debug -spec symbian-sbsv2 test.pro
 make -j4 -w release-gcce
+
+cd $home
+git clone --depth=50 --branch=master git://github.com/WagicProject/wagic.git wagic
+cd wagic/projects/mtg
+ls
+
+qmake DEFINES+=USE_PHONON QT+=phonon -spec symbian-sbsv2 wagic-qt.pro
+make -j4 -w -f Makefile.Graphics release-gcce

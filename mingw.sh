fcbdeaa99fe7b9e014c755cad9cfa2a599b86410
azaka <yukkuri.shrine@gmail.com>
2016-01-22 20:35:29 +0800

    sanity check

--- mingw.sh
+++ mingw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo add-apt-repository --yes ppa:tobydox/mingw-x-trusty
 sudo apt-get update -qq
-sudo apt-get install -y gcc-mingw-w64-i686 mingw-x-freetype
+sudo apt-get install -y gcc-mingw-w64-i686 mingw-x-freetype mingw-x-libogg
 where gcc
 gcc -v

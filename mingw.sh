03e99f81d9cb5a4b53ad43e88c150d8564c02e50
azaka <yukkuri.shrine@gmail.com>
2016-01-22 21:31:08 +0800

    fix

--- mingw.sh
+++ mingw.sh
@@ -2,6 +2,6 @@
 sudo add-apt-repository --yes ppa:tobydox/mingw-x-trusty
 sudo apt-get update -qq
 sudo apt-cache search mingw
-sudo apt-get install -y gcc-mingw-w64-i686 mingw-x-freetype mingw-x-libogg mingw-x-zlib
+sudo apt-get install -y gcc-mingw-w64-i686 mingw32-x-freetype mingw32-x-libogg mingw32-x-zlib
 where gcc
 gcc -v

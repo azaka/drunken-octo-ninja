082678e0832008054e37ce5910705f0b1997cbe8
azaka <yukkuri.shrine@gmail.com>
2016-01-22 21:24:59 +0800

    fix

--- mingw.sh
+++ mingw.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo add-apt-repository --yes ppa:tobydox/mingw-x-trusty
 sudo apt-get update -qq
-sudo apt-cache search freetype
+sudo apt-cache search mingw
 sudo apt-get install -y gcc-mingw-w64-i686 mingw-x-freetype mingw-x-libogg mingw-x-zlib
 where gcc
 gcc -v

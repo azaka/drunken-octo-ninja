c05f125e27eb87ab7ea30b048d761a2a1a36599a
azaka <yukkuri.shrine@gmail.com>
2016-01-31 19:30:16 +0800

    update

--- mingw.sh
+++ mingw.sh
@@ -3,10 +3,10 @@ sudo add-apt-repository --yes ppa:tobydox/mingw-x-trusty
 sudo apt-get update -qq
 sudo apt-cache search mingw
 sudo apt-get install -y gcc-mingw-w64-i686 
-sudo apt-get install -y mingw32-x-freetype 
+sudo apt-get install -y mingw-x-sdl 
 #mingw32-x-libogg mingw32-x-zlib
 which gcc
 ls /usr/bin | grep "mingw"
 gcc -v
 i686-w64-mingw32-gcc -v
-i686-w64-mingw32-gcc -lfreetype
+#i686-w64-mingw32-gcc -lfreetype

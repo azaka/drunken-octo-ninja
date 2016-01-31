37971f73666aa5a9acdd3ba957f74f854bdeaafa
azaka <yukkuri.shrine@gmail.com>
2016-01-31 19:35:19 +0800

    update

--- mingw.sh
+++ mingw.sh
@@ -3,7 +3,7 @@ sudo add-apt-repository --yes ppa:tobydox/mingw-x-trusty
 sudo apt-get update -qq
 sudo apt-cache search mingw
 sudo apt-get install -y gcc-mingw-w64-i686 
-sudo apt-get install -y mingw-x-sdl 
+sudo apt-get install -y mingw32-x-sdl 
 #mingw32-x-libogg mingw32-x-zlib
 which gcc
 ls /usr/bin | grep "mingw"

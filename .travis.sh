fa4231de1f2f82f54f7f7a4a96ebae42eb180dd6
azaka <yukkuri.shrine@gmail.com>
2016-03-06 20:50:24 +0800

    update

--- .travis.sh
+++ .travis.sh
@@ -34,6 +34,7 @@ travis_install2() {
 travis_script() {
 	#aria2c -x 16 https://lh3.googleusercontent.com/yZSoa451q8rqxor5UU2dDTcFNgu4uC-HomfEUeabqWY=m22 -o b09
 	#ffmpeg -i b09 -q -1 -s 640x360 -c:v libx265 b09.mp4
+	[ -f ~/.dropbox_uploader ] && ./dropbox_uploader.sh upload t/results/ travis-artifacts/$TRAVIS_JOB_NUMBER/
 }
 
 travis_script2() {

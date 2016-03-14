16a11da2895ad5621218ead749466ebeb9de7bc6
azaka <yukkuri.shrine@gmail.com>
2016-03-14 16:15:20 +0800

    update

--- .travis.sh
+++ .travis.sh
@@ -36,19 +36,15 @@ process_url() {
 	url=$1
 	
 	aria2c -x 16 $url -o $in_file
-	out_file=$in_file-x265.mp4
-	ffmpeg -i $in_file -q -1 -s 640x360 -c:v libx265 $out_file
-	stat $out_file
+	out_file=$in_file-crf26-x265.mp4
+	ffmpeg -i $in_file -threads 4 -s 640x360 -c:a copy -c:v libx265 -x265-params crf=26 -preset veryslow $out_file.mp4
+	
 	chmod +x ./dropbox_uploader.sh
 	[ -f ~/.dropbox_uploader ] && ./dropbox_uploader.sh upload $out_file travis-artifacts/$TRAVIS_JOB_NUMBER/
 }
 
 travis_script() {
-	process_url https://lh3.googleusercontent.com/qZnxJDCDwDCRLzmm5cMKivJ1_bv_tC2SzTCoVY7IKkKYOeYEilcKOkh3POglCO19a5um=m22 w08
-	process_url https://lh3.googleusercontent.com/aInhl-u-MJi6aMvA-t0DbWp8c2B6nM2uNKBUKBn-N0xJhUg6eRqL4ca-TAK_QZL4kaoQ=m22 b10
-	process_url https://lh3.googleusercontent.com/BHBphBOgBIxabTuypGrb7nOhEP-G3w2XXT8Wv9ZtgpyQ7gMBPSbj6QthkagZkgEnLAPJ=m22 p10
-	process_url https://lh3.googleusercontent.com/8v98E5eisFGh87KtppAGi28ByWaIMNIAJhJu0ZAW0EFMgwJYLsbXZdDrLHvkJcfriiKq=m22 v07
-	process_url https://lh3.googleusercontent.com/_wDkMqBldx9FskesauwAa_CajH5CBCQgzvOyOQUgctS-BUkimw6as7X-_hFVnigYbRb9=m22 n12
+	process_url https://lh3.googleusercontent.com/cl9nraYR4OQ3OtVF0ZbIonEvzs7ISvRUgSIwUAK0h7E=m37 r13
 }
 
 travis_script2() {

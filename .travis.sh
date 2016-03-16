becf481358a54996825e380ae4115d251becb20d
azaka <yukkuri.shrine@gmail.com>
2016-03-16 20:35:35 +0800

    update

--- .travis.sh
+++ .travis.sh
@@ -49,7 +49,7 @@ file_from_url() {
 
 travis_script() {
 	file_from_url \
-	p12 https://lh3.googleusercontent.com/9zKR_bUU84Thv0N2LfH4VAt_Leo4UPc5J4wmdNBcCrP13Ype_WtY_6JYCujkzlt-9Gv5=m22
+	v09 https://lh3.googleusercontent.com/KZBSSmhWbob_jJVIyZ2Iz6q9MbMixHASUm_P7ztoIm4mHK2I4ASLf3HYexIAwlOfKOC4=m22
 }
 
 travis_script2() {

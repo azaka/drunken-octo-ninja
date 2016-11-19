ca5fc6223bbff363dec76b80152ae0b57f64b8c3
azaka <yukkuri.shrine@gmail.com>
2016-11-19 19:17:45 +0800

    Output config.log contents on configure error

--- .travis.sh
+++ .travis.sh
@@ -29,7 +29,7 @@ travis_script() {
 	
 	download_extract ftp://ftp.gnu.org/gnu/nettle/nettle-3.3.tar.gz nettle
 	cd nettle-3.3
-	CC=arm-none-symabianelf-gcc ./configure --prefix=$HOME/out --host=arm-none-symbianelf
+	CC=arm-none-symabianelf-gcc ./configure --prefix=$HOME/out --host=arm-none-symbianelf || cat config.log && exit 1
 }
 
 

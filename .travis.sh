6ef03a1c4bf46b84e3fbecc1ba91b654350a9727
azaka <yukkuri.shrine@gmail.com>
2016-11-19 18:05:20 +0800

    Get nettle source

--- .travis.sh
+++ .travis.sh
@@ -6,7 +6,7 @@ download_extract() {
 }
 
 travis_before_install() {
-	git submodule update --init --recursive
+	#git submodule update --init --recursive
 }
 
 travis_install() {
@@ -17,52 +17,10 @@ travis_install() {
 	sudo apt-get -y install ffmpeg
 }
 
-travis_install2() {
-	sudo apt-get update
-	sudo apt-get -y install aria2
-	sudo apt-get install lib32stdc++6 lib32bz2-1.0 -qq
-	download_extract https://github.com/xsacha/SymbianGCC/releases/download/4.8.3/gcc4.8.3_x86-64.tar.bz2 compiler.tar.bz2
-	download_extract https://github.com/xsacha/SymbianGCC/releases/download/4.8.3/ndk-new.tar.bz2 ndk.tar.bz2
-	export EPOCROOT=$(pwd)/SDKs/SymbianSR1Qt474/ SBS_GCCE483BIN=$(pwd)/gcc4.8.3_x86-64/bin
-	aria2c -x 16 https://github.com/hrydgard/ppsspp-ffmpeg/archive/master.zip -o master.zip
-	unzip -qq master.zip
-	ls
-	mv ppsspp-ffmpeg-master ffmpeg
-	cp ffmpeg/symbian/armv6/lib/* $EPOCROOT/epoc32/release/armv5/urel/
-}
-
-process_url() {
-	in_file=$2
-	url=$1
-	
-	aria2c -x 16 $url -o $in_file
-	out_file=$in_file.mp4
-	ffmpeg -i $in_file -threads 4 -s 640x360 -tune animation -crf 20 -c:a copy $out_file
-	
-	chmod +x ./dropbox_uploader.sh
-	[ -f ~/.dropbox_uploader ] && ./dropbox_uploader.sh upload $out_file travis-artifacts/$TRAVIS_JOB_NUMBER/
-}
-
-file_from_url() {
-	process_url $2 $1
-}
-
 travis_script() {
-	file_from_url \
-	z12 https://lh3.googleusercontent.com/k6c2QQfL35y6Mj_Pwe36zEzjYEAunBWeYY8trL7H-LkopJwGwoxqqKB-KWghSSGsmpsK=m22
+	download_extract ftp://ftp.gnu.org/gnu/nettle/nettle-3.3.tar.gz nettle
 }
 
-travis_script2() {
-	export EPOCROOT=$(pwd)/SDKs/SymbianSR1Qt474 COMPILERROOT=$(pwd)/gcc4.8.3_x86-64 
-	export SBS_GCCE483BIN=$COMPILERROOT/bin
-	PATH=$SBS_GCCE483BIN:$(pwd)/tools/sbs/bin:$EPOCROOT/epoc32/tools:$EPOCROOT/bin:$(pwd)/tools/sbs/linux-x86_64-libc2_15/bin:$PATH
-	
-	#chmod +x b.sh
-	#./b.sh
-	#chmod +x mingw.sh
-	#./mingw.sh
-
-}
 
 set -e
 set -x

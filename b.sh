fa14db5c71a2ea4c25f95504e6174cf3905da8c3
azaka <azaka@users.noreply.github.com>
2017-11-20 22:08:41 +0800

    specify library path

--- b.sh
+++ b.sh
@@ -11,6 +11,18 @@ RANLIB=$COMPILERROOT/bin/$CROSS-ranlib
 EPOCINC=$EPOCROOT/epoc32/include
 SYSCFLAGS="-march=armv6 -mapcs -mfpu=vfp -mfloat-abi=softfp -marm -DOPENSSL_NO_MD4 -D__SUPPORT_CPP_EXCEPTIONS__ -D_UNICODE -D__SYMBIAN32__ -D__EPOC32__ -D__MARM__ -D__EABI__ -DUNICODE -D__MARM_ARMV5__ -D__ARMV6__ -D__GCCE_4__ -D__GCCE_4_8__ -DNDEBUG -D__GCCE__ -D__SYMBIAN_STDCPP_SUPPORT__ -DSYMBIAN_ENABLE_SPLIT_HEADERS -D__XSI_VISIBLE -D__BSD_VISIBLE -DHAVE_LIBPTHREAD -include $EPOCINC/gcce/gcce.h -I$EPOCROOT/include -I$EPOCROOT/mkspecs/common/symbian -I$EPOCINC -I$EPOCINC/stdapis -I$EPOCINC/stdapis/sys -I$EPOCINC/stdapis/stlportv5 -I$EPOCINC/variant -I$EPOCINC/stdapis -include $EPOCINC/stdapis/openssl/opensslv.h"
 
+EPOCLIB=$EPOCROOT/epoc32/release/armv5
+LDFLAGS="-Wl,-rpath-link=$EPOCLIB/lib -L$EPOCLIB/lib -L$EPOCLIB/urel -L$COMPILERROOT/arm-none-symbianelf/lib -nostdlib \
+-Wl,--target1-abs,--no-undefined -nodefaultlibs \
+-Wl,-shared \
+-Wl,-Ttext,0x80000,-Tdata,0x1000000 \
+-Wl,--entry=_E32Startup -Wl,-u,_E32Startup \
+-l:libpthread.dso \
+-l:eexe.lib -l:usrt3_1.lib \
+-l:libcrt0.lib -l:libstdcppv5.dso \
+-l:libc.dso -l:libm.dso -l:euser.dso -l:stdnew.dso -l:drtaeabi.dso -l:dfpaeabi.dso -lsupc++ -lgcc \
+-l:libz.dso -l:libssl.dso -l:libcrypto.dso -l:libdl.dso"
+
 REPODIR=$(pwd)
 
 cd $home
@@ -20,23 +32,5 @@ LUADIR=lua-5.2.4
 cd $LUADIR
 EXTRA_CFLAGS="-DCLOCKS_PER_SEC=1000"
 make ansi CC="$CC $SYSCFLAGS $EXTRA_CFLAGS" RANLIB=$RANLIB AR="$AR rcu"
-sudo cp liblua.a $REPO/lua.lib
 make install INSTALL_TOP=$REPODIR/gcce/usr
-ls $REPODIR
-ls $REPODIR/gcce/usr
 
-cd $home
-ls
-git clone --branch=master git://github.com/fluorohydride/ygopro-core ygopro-core
-cd ygopro-core
-git checkout -b v1337 fadb219
-git checkout -b build e8420f
-git apply $REPODIR/patch
-git commit -am"patch"
-git merge v1337 > /dev/null 2>&1
-echo
-git revert 99e817 --no-edit
-# EXTRA_CFLAGS="-DBOOST_COMPILER_CONFIG='"boost/mpl/aux_/config/gcc.hpp"'"
-# $CC $SYSCFLAGS $EXTRA_CFLAGS -c *.cpp -I$LUADIR -O2 -fpermissive -std=c++11
-# $AR rcu ocgcore.lib *.o
-# sudo cp ocgcore.lib $REPO/ocgcore.lib

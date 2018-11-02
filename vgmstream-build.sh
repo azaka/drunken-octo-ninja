447de69948de582622d0fcb7021c466305a756e9
azaka <azaka@users.noreply.github.com>
2018-11-02 23:29:47 +0800

    rule out build errors that may arise when cross compiling

--- vgmstream-build.sh
+++ vgmstream-build.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+# builds under linux
+git clone https://github.com/kode54/vgmstream
+cd vgmstream
+git checkout 29c1e5
+
+sudo add-apt-repository ppa:nilarimogard/webupd8 -y
+sudo apt-get update && sudo apt-get install libmpg123-dev \
+libvorbis-dev audacious-dev libglib2.0-dev libgtk2.0-dev libpango1.0-dev
+
+./bootstrap
+./configure
+make -j7 -f Makefile.autotools
+
+# termux
 git clone --depth 1 https://github.com/termux/x11-packages && cd x11-packages && ./scripts/travis-build.sh gtk2
